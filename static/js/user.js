3db05e4eb70710784664012349add560dd857ba7
Ian Rose <ianrosedev@gmail.com>
2022-03-21 19:43:36 -0400

    Move sidebar to top on small devices

--- static/js/user.js
+++ static/js/user.js
@@ -9,3 +9,40 @@
     }
   }
 })();
+
+// On small screens have search on top of page
+(() => {
+  const boostrapSizeLg = 992;
+  const postsElement = document.getElementById("posts");
+  const sidebarElement = document.getElementById("sidebar");
+  let isSwapped = false;
+
+  if (!postsElement || !sidebarElement) {
+    return;
+  }
+
+  const swapElements = (element1, element2) => {
+    const copy = element2.cloneNode();
+
+    element1.parentNode.insertBefore(copy, element1);
+    element2.parentNode.insertBefore(element1, element2);
+    element2.parentNode.replaceChild(element2, copy);
+  };
+
+  window.addEventListener("load", () => {
+    if (window.innerWidth < boostrapSizeLg) {
+      swapElements(postsElement, sidebarElement);
+      isSwapped = true;
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < boostrapSizeLg && !isSwapped) {
+      swapElements(postsElement, sidebarElement);
+      isSwapped = true;
+    } else if (window.innerWidth >= boostrapSizeLg && isSwapped) {
+      swapElements(postsElement, sidebarElement);
+      isSwapped = false;
+    }
+  });
+})();
